947673168a24c247dfe17cfb329af098a093cb0a
Shivam Sham Londhe <shivamlondhe91@gmail.com>
2024-07-23 09:37:47 +0530

    i created new HomeController for home pase and implement in web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AppListController;
+use App\Http\Controllers\HomeController;
 use GuzzleHttp\Promise\Create;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
@@ -8,15 +9,13 @@ use App\Http\Controllers\LoginController;
 use App\Http\Controllers\LogoutController;
 use App\Http\Controllers\NumFormatController;
 
-Route::get('/', function () {
-    return view('home');
-})->name("home");
+Route::get('/', [HomeController::class, "index"])->name("home");
 Route::get('login', [LoginController::class, "index"])->name("login");
 Route::post('login/check', [LoginController::class, "check"])->name("login.check");
 Route::resource("user", UserController::class);
 Route::get("logout", [LogoutController::class, "index"])->name("logout");
 Route::get("applist", [AppListController::class, "index"])->name("applist");
 Route::get("numberformat", [NumFormatController::class, "index"])->name("numberformat");
-Route::get("test",function(){
+Route::get("test", function () {
     echo "test";
 });
